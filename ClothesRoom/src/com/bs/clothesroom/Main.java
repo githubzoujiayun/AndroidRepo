f45a57766484e4579b94fd1d2d01bdefed9a8e66
chao.qin <chao.qin@sim.com>
2015-03-09 18:03:26 +0800

    upload

--- ClothesRoom/src/com/bs/clothesroom/Main.java
+++ ClothesRoom/src/com/bs/clothesroom/Main.java
@@ -4,35 +4,44 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentTabHost;
 import android.view.Menu;
 
-public class Main extends GeneralActivity{
-	
-	FragmentTabHost mTabHost;
+public class Main extends GeneralActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		setupTabHost();
-	}
+    FragmentTabHost mTabHost;
 
-	private void setupTabHost() {
-		mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
-		mTabHost.setup(this,getSupportFragmentManager(),R.id.realtabcontent);
-		mTabHost.addTab(mTabHost.newTabSpec("HomePage").setIndicator(getString(R.string.tab_home_page)),HomePage.class,new Bundle());
-		mTabHost.addTab(mTabHost.newTabSpec("Rack").setIndicator(getString(R.string.tab_rack)),Rack.class,null);
-		mTabHost.addTab(mTabHost.newTabSpec("Collocation").setIndicator(getString(R.string.tab_collocation)),Collocation.class,null);
-		mTabHost.addTab(mTabHost.newTabSpec("Self").setIndicator(getString(R.string.tab_self)),SelfSettings.class,null);
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        setupTabHost();
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		return super.onCreateOptionsMenu(menu);
-	}
+    private void setupTabHost() {
+        mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
+        mTabHost.addTab(
+                mTabHost.newTabSpec("HomePage").setIndicator(
+                        getString(R.string.tab_home_page)), HomePage.class,
+                new Bundle());
+        mTabHost.addTab(
+                mTabHost.newTabSpec("Rack").setIndicator(
+                        getString(R.string.tab_rack)), Rack.class, null);
+        mTabHost.addTab(
+                mTabHost.newTabSpec("Collocation").setIndicator(
+                        getString(R.string.tab_collocation)),
+                Collocation.class, null);
+        mTabHost.addTab(
+                mTabHost.newTabSpec("Self").setIndicator(
+                        getString(R.string.tab_self)), SelfSettings.class, null);
+    }
 
-	@Override
-	public boolean onPrepareOptionsMenu(Menu menu) {
-		return super.onPrepareOptionsMenu(menu);
-	}
-	
-	
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
+    }
+
 }
